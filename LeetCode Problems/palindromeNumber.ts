c475527e34930d3b969fdb427fbdaba665b70d08
Jacob Gramer <grameraggie@gmail.com>
2022-10-26 17:28:20 -0500

    Implements solution to Roman to Integer leetcode problem

--- LeetCode Problems/palindromeNumber.ts
+++ LeetCode Problems/palindromeNumber.ts
@@ -1,3 +1,5 @@
+
+// this version of the function iterates over the stringified number and tests the equality of the opposite indexes
 var isPalindrome = function(x: number) {
   let nums = String(x);
   for (let i=0; i<nums.length; i++) {
