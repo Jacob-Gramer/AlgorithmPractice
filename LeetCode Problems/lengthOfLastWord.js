2c3395bef30f4c722beebf92e6314f9ec4709ea0
Jacob Gramer <grameraggie@gmail.com>
2022-12-06 11:24:03 -0600

    Completes lengthOfLastWord.js function

---
+++ LeetCode Problems/lengthOfLastWord.js
@@ -0,0 +1,13 @@
+var lengthOfLastWord = function(s) {
+    debugger;
+    let word = '';
+    let list = s.split(' ');
+    for (let i=list.length - 1; i>0; i--) {
+        if (list[i] === '') continue
+        word = list[i];
+        return word.length;
+    }
+};
+
+const string = "a";
+console.log(lengthOfLastWord(string));
