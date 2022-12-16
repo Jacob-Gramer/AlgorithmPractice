5d4f29c23b1cc84e7eae01853094ead1efe23a06
Jacob Gramer <grameraggie@gmail.com>
2022-12-16 14:28:35 -0600

    Refactoring longestPalindrome

--- LeetCode Problems/longestPalindrome.js
+++ LeetCode Problems/longestPalindrome.js
@@ -3,15 +3,11 @@ var longestPalindrome = function(s) {
     if (s.length === 1) return s;
     if (s.length === 2 && s[0] !== s[1]) return s[0];
     let output = '';
-    // iterate over string using 2 pointers
     for (let i=0; i<s.length; i++) {
-    // decrement the rightmost pointer until the character is equal to the leftmost pointer
         let left = i, right = s.length - 1;
         let start = i, end = s.length - 1;
         let equal = false;
-        // while left pointer < right pointer
         while (left < right) {
-            // increment left and decrement right and compare equality
             if (s[right] !== s[left]) {
                 if (!equal) right--;
                 else {
