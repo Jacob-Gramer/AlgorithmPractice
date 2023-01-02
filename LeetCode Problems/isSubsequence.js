761015015d89236080a9ac18905803bbbff504fc
Jacob Gramer <grameraggie@gmail.com>
2023-01-02 14:21:44 -0600

    Implements two-pointer solution for Is Subsequence leetcode problem. 392 in isSubsequence.js

--- LeetCode Problems/isSubsequence.js
+++ LeetCode Problems/isSubsequence.js
@@ -1,19 +1,35 @@
+// var isSubsequence = function(s, t) {
+//     let sList = s.split(''),
+//         tList = t.split('');
+//     let i = 0;
+//     while (i < tList.length && sList.length > 0) {
+//         if (sList[0] === tList[i]) {
+//             if (i === 0) tList.splice(0, 1);
+//             else tList.splice(0, i+1);
+//             sList.splice(0, 1);
+//             i = 0;
+//         } else {
+//             i++;
+//         }
+//     }
+//     if (sList.length > 0) return false;
+//     return true;
+// };
+
+
+// Two-Pointer Solution
 var isSubsequence = function(s, t) {
-    let sList = s.split(''),
-        tList = t.split('');
-    let i = 0;
-    while (i < tList.length && sList.length > 0) {
-        if (sList[0] === tList[i]) {
-            if (i === 0) tList.splice(0, 1);
-            else tList.splice(0, i+1);
-            sList.splice(0, 1);
-            i = 0;
+    let p1 = 0, p2 = 0;
+    while(p1<s.length && p2<t.length) {
+        if (s[p1] === t[p2]) {
+            p1++;
+            p2++;
         } else {
-            i++;
-        }
+            p2++;
+        };
     }
-    if (sList.length > 0) return false;
-    return true;
+    if (p1 === s.length) return true;
+    return false
 };
 
 isSubsequence('bb', 'ahbgdc');
